2ea0873db5bf1e47d7313443f0c5900f71ed3089
chen <gakiclin@gmail.com>
2017-07-22 23:33:15 +0800

    update test server

--- test-server.js
+++ test-server.js
@@ -5,10 +5,12 @@
 
 const Koa = require('koa')
 const _ = require('koa-route')
+const bodyParser = require('koa-bodyparser')
 const logger = require('./index')
 
 const app = new Koa()
 app.use(logger())
+app.use(bodyParser())
 
 app.use(_.get('/200', function (ctx) {
   ctx.body = 'hello world'
@@ -24,7 +26,11 @@ app.use(_.get('/304', function (ctx) {
 
 app.use(_.get('/404', function (ctx) {
   ctx.status = 404
-  ctx.body = 'not found'
+  ctx.body = { learn: 'forever' }
+}))
+
+app.use(_.post('/hello', function (ctx) {
+  ctx.body = { learn: 'forever', bb: { a: 'xxx', c: { aaa: 111 }}}
 }))
 
 app.use(_.get('/500', function (ctx) {
@@ -35,5 +41,7 @@ app.use(_.get('/500', function (ctx) {
 app.use(_.get('/error', function (ctx) {
   throw new Error('oh no')
 }))
+
+app.listen(3008)
 
 module.exports = app
